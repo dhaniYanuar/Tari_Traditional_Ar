5e3de8522646681f40cfeec2c2f86d6aafba58db
Dhani Yanuar <dhani.y7@gmail.com>
2019-05-23 15:29:28 +0700

    Add Project

---
+++ Project_Tradence_AR/Assets/Scripts/EventHandler/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Nama Scene
+    public string SceneName;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("SceneName");
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Project_Tradence_AR/Assets/Scripts/EventHandler/TrackEventHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class TrackEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    public VideoPlayer videoCanvas;
+    public VideoPlayer videoTracker;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+        videoCanvas.Stop();
+        videoTracker.Stop();
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+    protected virtual void OnTrackingFound()
+    {
+        // Was already successfully done
+        videoCanvas.Play();
+        videoTracker.Play();
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        // Was already successfully done
+        videoCanvas.Pause();
+        videoTracker.Pause();
+    }
+}
